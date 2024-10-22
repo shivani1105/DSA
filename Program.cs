358ac80a1682b65d180f3ba29d450d1db310b479
shivani1105 <shivanireddy1105@gmail.com>
2024-10-22 16:32:33 -0500

    two sum

--- Program.cs
+++ Program.cs
@@ -81,6 +81,27 @@ class Program
 
         Console.WriteLine("\nSearching for key 105:");
         hasher.SearchByKey(105);
+
+
+        TwoSumSolution solution = new TwoSumSolution();
+
+        // Example 1: nums = [2,7,11,15], target = 9
+        int[] nums1 = { 2, 7, 11, 15 };
+        int target1 = 9;
+        int[] result1 = solution.TwoSum(nums1, target1);
+        Console.WriteLine($"Indices: [{result1[0]}, {result1[1]}]");
+
+        // Example 2: nums = [3,2,4], target = 6
+        int[] nums2 = { 3, 2, 4 };
+        int target2 = 6;
+        int[] result2 = solution.TwoSum(nums2, target2);
+        Console.WriteLine($"Indices: [{result2[0]}, {result2[1]}]");
+
+        // Example 3: nums = [3,3], target = 6
+        int[] nums3 = { 3, 3 };
+        int target3 = 6;
+        int[] result3 = solution.TwoSum(nums3, target3);
+        Console.WriteLine($"Indices: [{result3[0]}, {result3[1]}]");
     }
 
 }
---
+++ TwoSumSolution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA
+{
+    internal class TwoSumSolution
+    {
+        // Method to find two indices that add up to the target
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> store = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int remainder = target - num;
+
+                // If the remainder exists in the dictionary, return the indices
+                if (store.ContainsKey(remainder))
+                {
+                    return new int[] { store[remainder], i };
+                }
+
+                // If remainder doesn't exist, store the current number with its index
+                if (!store.ContainsKey(remainder))
+                {
+                    store[num] = i;
+                }
+            }
+
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
